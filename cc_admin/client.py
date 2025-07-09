3bbb505768c3c64bdce4dc0fc4d4187ed44d5b11
boris098765 <sites-rakitin@yandex.ru>
2025-07-09 12:07:15 +0300

    Добавил эндпоинты в клиент

--- cc_admin/client.py
+++ cc_admin/client.py
@@ -3,6 +3,13 @@ import requests
 from .endpoints import (
     users,
     roles,
+    permissions,
+    api_modes,
+    model_cats,
+    models,
+    capabilities,
+    text_model_configs,
+    model_pricings
 )
 from .api_response import ApiResponse
 
@@ -15,8 +22,15 @@ class CCAPIClient:
         self.base_url = base_url
         self.token = token
 
-        self.users = users.UsersEndpoint(self)
-        self.roles = roles.RolesEndpoint(self)
+        self.users              = users.UsersEndpoint(self)
+        self.roles              = roles.RolesEndpoint(self)
+        self.permissions        = permissions.PermissionsEndpoint(self)
+        self.api_modes          = api_modes.ApiModesEndpoint(self)
+        self.model_cats         = model_cats.ModelCatsEndpoint(self)
+        self.models             = models.ModelsEndpoint(self)
+        self.capabilities       = capabilities.CapabilitiesEndpoint(self)
+        self.text_model_configs = text_model_configs.TextModelConfigEndpoint(self)
+        self.model_pricings     = model_pricings.ModelPricingsEndpoint(self)
 
     def request(self, path, **kwargs) -> ApiResponse:
         url = f"{self.base_url}/{path.lstrip('/')}"
